ed9eb1fe47e90492fcd8a0759262344309d58d79
Arthur <arthur@utopios.solutions>
2023-03-20 12:15:13 +0100

    template

--- django/demos/myblog/blog/urls.py
+++ django/demos/myblog/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
+    # path prend en paramètre la route, une vue, et un nom (facultatif)
     path('', views.index, name="index"),
-    path('<int:num>', views.number, name="number")
+    path('<int:num>', views.number, name="number"),
+    path('test', views.test_template, name="test"),
 ]
--- django/demos/myblog/blog/views.py
+++ django/demos/myblog/blog/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): 
-    return HttpResponse("hello world")
+
+    villes = ['paris', 'roubaix', 'toulouse']
+
+    return render(request, 'blog/index.html', {'prenom': 'arthur', 'villes': villes})
 
 def number(request, num):
     return HttpResponse(f"{num}")
+
+def test_template(request):
+    return render(request, 'base.html')
